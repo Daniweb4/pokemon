4d28f7984883f27fb7e870a48f1b768666063b30
Danilo Montecel <daniloomarmontecel@gmail.com>
2024-03-10 19:20:58 -0500

    Verificacion1

--- src/pages/WelcomePages.jsx
+++ src/pages/WelcomePages.jsx
@@ -17,7 +17,7 @@ const WelcomePage = () => {
   return (
     <section className='container'>
       
-      <img className='img2' src='https://s3-alpha-sig.figma.com/img/ca59/d9ce/98042af437fdff212d3259040db2e2db?Expires=1708905600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=hNCQVLq~HPorbBWb4rEkOqie9smUE1HY8Cb4c0U-Od6iZufMAZQNm-kiHFau1sqvWbnuMDqz00XxvpeDx~upL99yrN~mm71CmS9y2lUfh-5-E6P9ydl-~alyCUW9x5XdGWzzV9Kb9BBrelRjmN~NOUYOvT3RbcLlNmaZ5QklXVBgRWz3vZLIOgWCltdQlsJLJu6kMNl8WbPk8ILz5rdqjOpSWIsMhSWXu2MUZFeEidcdtlBDB5kdEqceNZciCZdO5e9SHTDjWlZZUYqzNE82UFMSIJlWqUPHagbWFrnBogtYWDdrJTpm~cmwsTL8RCPZjAnWMZscgB0yOGCIi8jKSg__'/>
+      <img className='img2' src='https://s3-alpha-sig.figma.com/img/ca59/d9ce/98042af437fdff212d3259040db2e2db?Expires=1711324800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=iRm6vCpmOAn20ysvawHlB7ERW2ZMFUUkbtda2y17qGtEq3E-8lOwGuct5rFCJosRRTBsCvvE033UJH2iVoXCr1IUYwRgBbNKXbK9KQTWZwNgxzlCyVOWs9q6hxJ6mzNj9UewW3IcM0b80LYopqHH0V2cH7TFcpeskFvUjN3tRiCIYoByY2gjNS9gEIJgJjDWGNM6LXV1-EeKiLCzC~AKGxsadyIj53LlJ1i5rrL7nC03w7xi2u0j8S1WS2JyZStL4xPus0J0FYa4PUrD9M7RC5xFSqqhRub35cJmLnM1LzMbDhyJNO~JQY7qppJl-ThCH-evNAkgg-kdI0vhMZ1ZGw'/>
          <h1 className='welcome'>¡Hola entrenador!</h1>
          <p className='data'>Para poder comenzar dame tu nombre</p>
          <form className='form1' onSubmit={handelSubmit}>
